ff1e8a0383f0512204cc09673676fbce743a1e9b
Rahma Setiani <rahmasetiani200@gmail.com>
2024-10-03 10:32:07 +0700

    upload

--- db/koneksi.php
+++ db/koneksi.php
@@ -8,6 +8,7 @@ $database = "pendaftaran_beasiswa";
 // Membuat koneksi ke database
 $conn = new mysqli($servername, $username, $password, $database);
 
+
 // Mengecek koneksi
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
